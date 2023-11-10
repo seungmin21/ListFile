c10629091293074ba8b05d18759762d4ac48fb9a
ohseungmin <ohmin2886@gmail.com>
2023-11-10 18:22:20 +0900

    같은 이름의 요소 중에 마지막 요소의 위치 출력

---
+++ List6.js
@@ -0,0 +1,12 @@
+// 명단에 같은 이름(성씨제외)을 가진 학생이 두 명 있습니다. 이 중 마지막에 있는 학생의 위치를 찾아 출력하는 코드를 작성하세요.
+// 같은 이름과 마지막 학생 위치 = lastindexof()
+
+const studentList = [
+  "a",
+  "b",
+  "c",
+  "b",
+  "e",
+];
+
+console.log(studentList.lastIndexOf("b"))
