877410487042775278f956b8ea3eb0284c6d274b
ohseungmin <ohmin2886@gmail.com>
2023-11-13 21:24:27 +0900

    모든 요소 확인 every

---
+++ List11.js
@@ -0,0 +1,24 @@
+const studentList = [
+    "김우진",
+    "김현",
+    "방승희",
+    "변호녕",
+    "소사무엘",
+    "송영준",
+    "신동현",
+    "오승민",
+    "유승민",
+    "윤준현",
+    "이민구",
+    "이유안",
+    "이은정",
+    "정영식",
+    "최성민",
+    "최은철",
+    "홍문기"
+  ];
+
+// 모든 학생들의 이름의 길이가 세 글자인지 확인하는 코드
+const allStudentsHaveThreeLetterNames = studentList.every((name) => name.length === 3);
+
+console.log(allStudentsHaveThreeLetterNames); // false
